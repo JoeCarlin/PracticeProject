a26d6db0a4c6e7cd677ec546b3d80b5a580e0d44
Joe Carlin <joecarlin30@gmail.com>
2025-01-11 10:34:57 -0500

    Update WeightConverter.py

--- ProjectFiles/WeightConverter.py
+++ ProjectFiles/WeightConverter.py
@@ -6,10 +6,11 @@ unit = input("Kiolograms or Pounds? (L or K): ")
 if unit == "K":
     weight = weight * 2.205
     unit = "Lbs"
+    print(f"Your weight is {round(weight, 1)} {unit}")
 elif unit == "L":
     weight = weight / 2.205
     unit = "Kgs"
+    print(f"Your weight is {round(weight, 1)} {unit}")
 else:
     print("Invalid unit")
 
-print(f"Your weight is {round(weight, 1)} {unit}")
