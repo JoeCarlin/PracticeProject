da233e1d2483b94cca0aa04df3c36718688c6d00
Joe Carlin <joecarlin30@gmail.com>
2025-01-12 20:41:22 -0500

    Update car.py

--- ProjectFiles/car.py
+++ ProjectFiles/car.py
@@ -13,3 +13,10 @@ class Car:
 
     def describe(self):
         print(f"{self.year} {self.make} {self.model} in {self.color}.")
+
+
+car1 = Car("Toyota", "Corolla", 2015, "Red")
+car2 = Car("Honda", "Civic", 2016, "Blue")
+car3 = Car("Ford", "Mustang", 2014, "Black")
+
+car3.describe()
