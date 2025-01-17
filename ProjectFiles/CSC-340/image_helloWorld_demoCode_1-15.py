158c74cbfc5a450f5a50116fa6ab7eb7226762d0
Joe Carlin <joecarlin30@gmail.com>
2025-01-17 11:42:23 -0500

    update

--- ProjectFiles/CSC-340/image_helloWorld_demoCode_1-15.py
+++ ProjectFiles/CSC-340/image_helloWorld_demoCode_1-15.py
@@ -1,48 +1,41 @@
 import cv2
 import numpy as np
 
-# read in an image in opencv
+# Read in an image using OpenCV
 image = cv2.imread("ProjectFiles/CSC-340/Media/cones1.png")
-# if image is in a subfolder, give relative path
-#image = cv2.imread("img/cones1.png")
+# If image is in a subfolder, give relative path
+# image = cv2.imread("img/cones1.png")
 
-# get size of an image
-numRows = image.shape[0] # height of image
-numCols = image.shape[1] # width of image
-print("size: ", numRows, numCols)
+# Get size of the image (height and width)
+numRows = image.shape[0]  # Height of the image
+numCols = image.shape[1]  # Width of the image
+print("Original size: ", numRows, numCols)
 
-# create a second image, of the same size as the first
-emptyIm = np.zeros( (numRows, numCols, 3), np.float32)
+# --- Scaling factor based on original image dimensions ---
+scale_factor = 2  # Example scaling factor, change as needed
+new_numRows = int(numRows * scale_factor)
+new_numCols = int(numCols * scale_factor)
 
+# --- Create new larger image of the correct size ---
+# Create a new larger image with a black background (empty image)
+emptyIm = np.zeros((new_numRows, new_numCols, 3), np.float32)
 
-# iterate over all the pixels in the image
-for i in range(numRows): # height of the image, y coordinates
-    for j in range(numCols): # width of the image, x coordinates
+# --- Copy the original image into the center of the new image ---
+for i in range(numRows):
+    for j in range(numCols):
+        # Place original image at the center of the new image
+        emptyIm[i + (new_numRows - numRows) // 2][j + (new_numCols - numCols) // 2] = image[i, j]
 
+# --- Gradient effect for one color channel (x-gradient for red) ---
+# Create a gradient effect along the x-direction for the red channel
+for i in range(new_numRows):
+    for j in range(new_numCols):
+        emptyIm[i, j, 2] = (j / new_numCols) * 255  # Red channel (x-gradient)
 
-        avgInt = (0.082 * float(image[i][j][0]) + 0.609 * float(image[i][j][1]) + 0.309 * float(image[i][j][2]))
-        # copy the blue channel into empty image
-        emptyIm[i][j][0] = avgInt
+# --- Load, initialize, display, and save images correctly ---
+cv2.imshow("New Image with Original in Center", emptyIm / 255.0)
+cv2.imwrite("ProjectFiles/CSC-340/Media/scaled_with_gradient.png", emptyIm)
 
-        # copy the green channel into empty image
-        emptyIm[i][j][1] = avgInt
-
-        # copy the green channel into empty image
-        emptyIm[i][j][2] = avgInt
-
-        # image[i][j][0]: blue channel at location row i, col j
-        # image[i][j][1]: green channel at location row i, col j
-        # image[i][j][2]: red channel at location row i, col j
-    
-
-
-
-# display an image
-cv2.imshow("Displaying an image", emptyIm/255.0)
-
-# save an image
-cv2.imwrite("ProjectFiles/CSC-340/Media/savedImg_sca.png", emptyIm)
-
-
-cv2.waitKey(0) # not going to proceed until you hit "enter"
-cv2.destroyAllWindows() # closes all windows opened with "imshow"
+# Wait for a key press to close the windows
+cv2.waitKey(0)
+cv2.destroyAllWindows()
