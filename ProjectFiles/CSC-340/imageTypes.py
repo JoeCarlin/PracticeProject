e9f1341ccc56e45abcc5e3481deef921989a2e34
Joe Carlin <joecarlin30@gmail.com>
2025-01-29 13:31:44 -0500

    update

--- ProjectFiles/CSC-340/imageTypes.py
+++ ProjectFiles/CSC-340/imageTypes.py
@@ -6,12 +6,11 @@ image1 = cv2.imread("ProjectFiles/CSC-340/Media/cones1.png")
 numRows = image1.shape[0]
 numCols = image1.shape[1]  
 
-image2 = np.zeros((numRows, numCols, 3), np.uint8)  
+image2 = np.zeros((numRows, numCols, 3), np.float32)
 
 for i in range(numRows):
     for j in range(numCols):
-        image2[i][j] = image1[i][j]
-    
+        image2[i][j] = image1[i][j] / 255.0  # Normalize values
 
 cv2.imshow("Image 2", image2)
 cv2.waitKey(0)
