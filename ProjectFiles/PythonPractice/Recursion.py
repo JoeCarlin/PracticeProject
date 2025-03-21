78c2542a3f6a662860248757d8a9b28a42b55050
Joe Carlin <joecarlin30@gmail.com>
2025-03-21 11:21:14 -0400

    Update Recursion.py

--- ProjectFiles/PythonPractice/Recursion.py
+++ ProjectFiles/PythonPractice/Recursion.py
@@ -14,4 +14,44 @@ def OddNums(num):
         print(num)
         return OddNums(num - 2)
 
-OddNums(6) 
+#OddNums(7)
+
+def Factorial(num):
+    if num == 1:
+        return 1
+    else:
+        return num * Factorial(num - 1)
+
+#print(Factorial(6))
+
+def SumList(lst):
+    if len(lst) == 1:
+        return lst[0]
+    else :
+        return lst[0] + SumList(lst[1:])
+
+#print(SumList([1,2,3,4,5,6,7,8,9,10]))
+
+def maxList(lst):
+    if len(lst) == 1:
+        return lst[0]
+    else:
+        return max(lst[0], maxList(lst[1:]))
+
+#print(maxList([1,2,3,4,5,6,7,8,9,10]))
+
+def minList(lst):
+    if len(lst) == 1:
+        return lst[0]
+    else:
+        return min(lst[0], minList(lst[1:]))
+
+#print(minList([1,2,3,4,5,6,7,8,9,10]))
+
+def reverseList(lst):
+    if len(lst) == 1:
+        return lst
+    else:
+        return reverseList(lst[1:]) + [lst[0]]
+
+print(reverseList([1,2,3,4,5,6,7,8,9,10]))
