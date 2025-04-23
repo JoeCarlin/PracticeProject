11973ef761670388af5da89ce74b155151eef552
Joe Carlin <joecarlin30@gmail.com>
2025-04-23 10:31:49 -0400

    Update HelloWorld.go

--- ProjectFiles/GOPractice/HelloWorld.go
+++ ProjectFiles/GOPractice/HelloWorld.go
@@ -1,3 +1,24 @@
-// print hello world
+// BasicSyntax.go
 package main
 
+import "fmt"
+
+// Main function: Entry point of the program
+func main() {
+	// Variable declarations
+	var message string = "Hello, Go!"
+	var number int = 42
+	isGoFun := true // Short variable declaration
+
+	// Print statements
+	fmt.Println(message)
+	fmt.Printf("The number is: %d\n", number)
+	fmt.Printf("Is Go fun? %t\n", isGoFun)
+
+	// Conditional statement
+	if isGoFun {
+		fmt.Println("Yes, Go is fun!")
+	} else {
+		fmt.Println("No, Go is not fun.")
+	}
+}
